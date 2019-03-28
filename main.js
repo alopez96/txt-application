92ab151823d26c7f56564e1c55f4f795baa6b5c0
Arturo <alopez96@ucsc.edu>
2019-03-28 10:15:09 -0700

    app fully functional

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const express = require('express');
 
-const app = express();
+function myFunction() {
+    var num1, num2, num3;
+    // Get the values of the input fields
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    num3 = document.getElementById("num3").value;
 
-app.use(express.static(__dirname))
+    var array;
+    //JS sort function reads string 10 less than 3 because 1 < 3,
+    //using callback to compare the numbers as num instead of string
+    array = [num1, num2, num3].sort(function(a,b){
+        return a - b;
+    });
+    array.toString();
 
-app.listen(3000);
+    //assign result to id for display
+    document.getElementById("result").innerHTML = array;
+  }
